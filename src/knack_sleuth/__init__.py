0a7b4e431e286512baec0e3ddf4fd878ba7c2ee6
mcmasty@outcastsystems.com <mcmasty@outcastsystems.com>
2025-10-30 16:44:20 -0400

    Adding JSON Schema export of KnackAppMetadata

--- src/knack_sleuth/__init__.py
+++ src/knack_sleuth/__init__.py
@@ -1,6 +1,6 @@
 """KnackSlueth - Find usages of data objects in Knack app metadata."""
 
-__version__ = "0.1.19"
+__version__ = "0.1.20"
 
 from knack_sleuth.core import load_app_metadata
 from knack_sleuth.models import (
