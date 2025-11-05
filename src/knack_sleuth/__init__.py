4d71251d45629c0e4bb56c5c0e179e8bd318990e
mcmasty@outcastsystems.com <mcmasty@outcastsystems.com>
2025-11-05 15:08:34 -0500

    Adding DB Schema export

--- src/knack_sleuth/__init__.py
+++ src/knack_sleuth/__init__.py
@@ -1,6 +1,6 @@
 """KnackSlueth - Find usages of data objects in Knack app metadata."""
 
-__version__ = "0.1.20"
+__version__ = "0.2.0"
 
 from knack_sleuth.core import load_app_metadata
 from knack_sleuth.models import (
