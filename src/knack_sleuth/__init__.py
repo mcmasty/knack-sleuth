c3377c8895f718aec9cf07f43895909adfc7b894
mcmasty <mcmasty@outcastsystems.com>
2026-02-06 21:40:42 -0500

    add claude skill installation

--- src/knack_sleuth/__init__.py
+++ src/knack_sleuth/__init__.py
@@ -1,6 +1,6 @@
 """KnackSlueth - Find usages of data objects in Knack app metadata."""
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 from knack_sleuth.core import load_app_metadata
 from knack_sleuth.models import (
